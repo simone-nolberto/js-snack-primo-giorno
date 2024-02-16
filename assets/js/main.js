8344f771fd143acc9601fa71cf57c283e1de9f64
simone-nolberto <simone.nolberto@outlook.com>
2024-02-16 16:20:32 +0100

    add
    esercizio 2c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
 // esercizio 1
 
-let number = 100 //number
+let number = 100; //number
 
 console.log(number);
 
 // esercizio 2
 
-const PI = "3.14" //number
+const PI = "3.14"; //number
 
 console.log(PI);
+
+
+// esercizio 2/B
+
+//PI = 3.1416; //number
+
+//la console restituisce un TypeError (impossibile cambiare il valore ad una costante)
+
+
+// esercizio 2/C
+
+let radius = 8; //number
+
+let circle = (radius * 2 * PI) //operation
+
+console.log(circle);
